af2e30281afae09ad1897e846c82c9937f4829c0
Colin Jaffe <balloonasaurus@gmail.com>
2021-04-07 08:47:50 -0400

    Finishes basic solution.

--- get-order-total.js
+++ get-order-total.js
@@ -1,4 +1,10 @@
-const getOrderTotal = (order) => {
+const stateTaxes = {
+  NY: 5,
+  CA: 8,
+  WAS: 10,
+};
+
+const getOrderTotal = (order, state) => {
   let total = 0;
   for (const item of order) {
     let quantity = 1;
@@ -6,10 +12,28 @@ const getOrderTotal = (order) => {
       quantity = item.quantity;
     }
 
-    total += (item.price + item.shipping) * quantity;
+    total += item.price * quantity;
   }
 
-  return total;
+  if (total <= 100) {
+    for (const item of order) {
+      let quantity = 1;
+      if (item.quantity) {
+        quantity = item.quantity;
+      }
+
+      if (item.shipping) {
+        total += item.shipping * quantity;
+      }
+    }
+  }
+
+  let taxMultiplier = 1;
+  if (state) {
+    taxMultiplier = stateTaxes[state] / 100 + 1;
+  }
+
+  return Math.floor(total * taxMultiplier * 100) / 100;
 };
 
 module.exports = getOrderTotal;
--- get-order-total.test.js
+++ get-order-total.test.js
@@ -1,11 +1,5 @@
 const getOrderTotal = require("./get-order-total");
 
-const stateTaxes = {
-  NY: 5,
-  CA: 8,
-  WAS: 10,
-};
-
 const order1 = [
   {
     name: "Charmander",
@@ -59,9 +53,136 @@ if (result2 !== 40) {
 // 3. An order where state taxes are multiplied at the end.
 //    Note that the best way to handle that is to take in another parameter to our function!
 //    (It wouldn't make sense for the state to depend on the item's state, but the customer's.)
+//
+
+const order3 = [
+  {
+    name: "Charmander",
+    price: 8,
+    shipping: 8,
+  },
+
+  {
+    name: "Pikachu",
+    price: 5,
+    shipping: 3,
+  },
+];
+
+const order4 = [
+  {
+    name: "Charmander",
+    price: 8,
+    shipping: 8,
+  },
+
+  {
+    name: "Pikachu",
+    price: 5,
+    shipping: 3,
+  },
+];
+
+const stateTaxes = {
+  NY: 5,
+  CA: 8,
+  WAS: 10,
+};
+
+const result3 = getOrderTotal(order3, "NY");
+if (result3 !== 25.2) {
+  throw new Error(`Expected: 25.20. Got: ${result3}`);
+}
+
+const result4 = getOrderTotal(order4, "CA");
+if (result4 !== 25.92) {
+  throw new Error(`Expected: 25.92. Got: ${result4}`);
+}
 
 // 4. An order where some items are digital, i.e., have no shipping cost.
 
+const order5 = [
+  {
+    name: "Charmander",
+    price: 8,
+  },
+
+  {
+    name: "Pikachu",
+    price: 5,
+    shipping: 3,
+  },
+];
+
+const order6 = [
+  {
+    name: "Charmander",
+    price: 8,
+    shipping: 8,
+  },
+
+  {
+    name: "Pikachu",
+    price: 5,
+  },
+];
+
+const result5 = getOrderTotal(order5);
+if (result5 !== 16) {
+  throw new Error(`Expected: 16 and got: ${result5}`);
+}
+
+const result6 = getOrderTotal(order6);
+if (result6 !== 21) {
+  throw new Error(`Expected: 21. Got: ${result6}`);
+}
+
 // 5. An order where the price PRE-shipping is above 100, in which case we want to remove shipping altogether.
+const order7 = [
+  {
+    name: "Charmander",
+    price: 8,
+  },
+
+  {
+    name: "MewTwo",
+    price: 90,
+  },
+
+  {
+    name: "Pikachu",
+    price: 5,
+    shipping: 3,
+  },
+];
+
+const order8 = [
+  {
+    name: "Charmander",
+    price: 8,
+    shipping: 8,
+  },
+
+  {
+    name: "MewTwo",
+    price: 100,
+  },
+
+  {
+    name: "Pikachu",
+    price: 5,
+    shipping: 8,
+  },
+];
+
+const result7 = getOrderTotal(order7);
+if (result7 !== 103) {
+  throw new Error(`Expected: 103 and got: ${result7}`);
+}
+
+const result8 = getOrderTotal(order8);
+if (result8 !== 113) {
+  throw new Error(`Expected: 113. Got: ${result8}`);
+}
 
 console.log("All tests have passed!");
